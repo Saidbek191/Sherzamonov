1ca8f59645ff7f971b59a625ccd7dc524372831f
Saidbek-Kodirov <qodirovsk@gmail.com>
2026-09-21 12:16:12 +0200

    style: format project with Prettier (Mon, Sep 21, 2026 12:16:12 PM)

--- js/script.js
+++ js/script.js
@@ -1,137 +1,138 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  const menuBtn = document.querySelector(".head__menu");
+  const nav = document.querySelector(".head__nav");
 
-	const menuBtn = document.querySelector('.head__menu');
-	const nav = document.querySelector('.head__nav');
-	
-	menuBtn?.addEventListener('click', () => {
-		menuBtn.classList.toggle('active');
-		nav.classList.toggle('active');
-		document.body.classList.toggle('lock');
-	});
+  menuBtn?.addEventListener("click", () => {
+    menuBtn.classList.toggle("active");
+    nav.classList.toggle("active");
+    document.body.classList.toggle("lock");
+  });
 
-	// CARUSEL
+  // CARUSEL
 
-	const posts = document.querySelectorAll('.post-box');
+  const posts = document.querySelectorAll(".post-box");
 
+  const left = {
+    img: document.querySelector(".columns__img"),
+    date: document.querySelector(".columns__left .columns__date"),
+    title: document.querySelector(".columns__left .columns__subtitle"),
+    text: document.querySelector(".columns__left .columns__text"),
+    btn: document.querySelector(".columns__left .columns__btn"),
+  };
 
-const left = {
-	img: document.querySelector('.columns__img'),
-	date: document.querySelector('.columns__left .columns__date'),
-	title: document.querySelector('.columns__left .columns__subtitle'),
-	text: document.querySelector('.columns__left .columns__text'),
-	btn: document.querySelector('.columns__left .columns__btn')
-};
+  function updateLeft(post) {
+    if (!post) return;
+    left.img.src = post.dataset.img;
+    left.date.innerHTML = post.dataset.date;
+    left.title.innerHTML = post.dataset.title;
+    left.text.innerHTML = post.dataset.text;
+    left.btn.href = post.dataset.link;
 
-	function updateLeft (post) {
-		if (!post) return;
-		left.img.src = post.dataset.img;
-		left.date.innerHTML = post.dataset.date;
-		left.title.innerHTML = post.dataset.title;
-		left.text.innerHTML = post.dataset.text;
-		left.btn.href = post.dataset.link;
-		
-		posts.forEach(p => p.classList.remove('active'));
-		post.classList.add('active');
-	}
-	
-	posts.forEach(post => post.addEventListener('click', () => updateLeft(post)));
-		updateLeft(posts[0]);
+    posts.forEach((p) => p.classList.remove("active"));
+    post.classList.add("active");
+  }
 
-		
-		let currentLang = localStorage.getItem('lang') || "en";
-			
-		async function changeLanguage(lang, button) {
-			// Снимаем класс "active" со всех кнопок
-			document.querySelectorAll('.lang__switch').forEach(btn =>	btn.classList.remove('active'));
-			// Добавляем "active" к нажатой кнопке
-			button?.classList.add('active');
-			
-			try {
-				const response = await fetch(`./locales/${lang}.json`);
-				const translations = await response.json();
-				
-				document.querySelectorAll('[data-i18n]').forEach(el => {
-					const key = el.dataset.i18n;
-					if(!translations[key]) return;
-					if(translations[key].includes('<')) el.innerHTML = translations[key];
-					else el.textContent = translations[key];
-		});
-		
-		localStorage.setItem('lang', lang);
-		currentLang = lang;
-		loadArticle(lang);
-	} catch	(error) {
-		console.error('Ошибка загрузки перевода:', error);
-	}
-}
+  posts.forEach((post) =>
+    post.addEventListener("click", () => updateLeft(post)),
+  );
+  updateLeft(posts[0]);
 
-	document.querySelectorAll('.lang__switch').forEach(btn => {
-		btn.addEventListener('click', () => changeLanguage(btn.dataset.lang, btn));
-	});
+  let currentLang = localStorage.getItem("lang") || "en";
 
-		const btn = document.querySelector(`.lang__switch[data-lang="${currentLang}"]`);
-		changeLanguage(currentLang, btn);
-		
-		
-		// Carousel-btn
-		const track = document.querySelector('.carousel-track');
-		const cards = document.querySelectorAll('.cards__row');
-		const prev = document.querySelector('.pre');
-		const next = document.querySelector('.nex');
-		let index = 0;
-		
-		
-		function updateCarousel () {
-	if (!cards.length || !track) return;
-	const gap = parseInt(getComputedStyle(track).columnGap) || 45;
-	const cardWidth = cards[0].offsetWidth + gap;
-	track.style.transform = `translateX(-${index * cardWidth}px)`;
-}
+  async function changeLanguage(lang, button) {
+    // Снимаем класс "active" со всех кнопок
+    document
+      .querySelectorAll(".lang__switch")
+      .forEach((btn) => btn.classList.remove("active"));
+    // Добавляем "active" к нажатой кнопке
+    button?.classList.add("active");
 
+    try {
+      const response = await fetch(`./locales/${lang}.json`);
+      const translations = await response.json();
 
-if(track && cards.length && prev && next) {
-	
-	next.addEventListener('click', () => {
-		if (index < cards.length - 1) {
-			index++;
-			updateCarousel();
-		}
-	});
-	
-	prev.addEventListener('click', () => {
-		if (index > 0) {
-			index--;
-			updateCarousel(); 
-		}
-	});
-	
-	window.addEventListener('resize', updateCarousel);
-	updateCarousel();
-}
+      document.querySelectorAll("[data-i18n]").forEach((el) => {
+        const key = el.dataset.i18n;
+        if (!translations[key]) return;
+        if (translations[key].includes("<")) el.innerHTML = translations[key];
+        else el.textContent = translations[key];
+      });
 
-// Изменение языка в статьях
+      localStorage.setItem("lang", lang);
+      currentLang = lang;
+      loadArticle(lang);
+    } catch (error) {
+      console.error("Ошибка загрузки перевода:", error);
+    }
+  }
 
-function markdownToHtml(md) {
-	if (!md) return '';
-	return md
-	.replace(/^### (.*$)/gim, '<h3 class="blog-post__title title">$1</h3>')
-	.replace(/^## (.*$)/gim, '<h2 class="blog-post__title title">$1</h2>')
-	.replace(/^# (.*$)/gim, '<h1 class="blog-post__title title">$1</h1>')
-	.replace(/\*\*(.+?)\*\*/gim, '<b>$1</b>')
-	.replace(/\*(.+?)\*/gim, '<i>$1</i>')
-	.replace(/^(?!<h|<p>)(.+)$/gim, '<p class="blog-post__text text">$1</p>')
-	.replace(/\n/gim, '<br>');
-}
+  document.querySelectorAll(".lang__switch").forEach((btn) => {
+    btn.addEventListener("click", () => changeLanguage(btn.dataset.lang, btn));
+  });
 
-function loadArticle (lang) {
+  const btn = document.querySelector(
+    `.lang__switch[data-lang="${currentLang}"]`,
+  );
+  changeLanguage(currentLang, btn);
 
-	const articleContainer = document.getElementById("article-container");
-	if (articleContainer) {
-		const currentArticlesSlug = articleContainer.dataset.slug;
-		fetch(`articles/${currentArticlesSlug}/${lang}.md`)
-		.then(res => res.ok ? res.text() : Promise.reject("File missing"))
-		.then(md =>	articleContainer.innerHTML = markdownToHtml(md))
-		.catch(() => articleContainer.innerHTML =	`<p>Перевод для языка "${lang}" недоступен.</p>`);	
-	}
-}
-		});
+  // Carousel-btn
+  const track = document.querySelector(".carousel-track");
+  const cards = document.querySelectorAll(".cards__row");
+  const prev = document.querySelector(".pre");
+  const next = document.querySelector(".nex");
+  let index = 0;
+
+  function updateCarousel() {
+    if (!cards.length || !track) return;
+    const gap = parseInt(getComputedStyle(track).columnGap) || 45;
+    const cardWidth = cards[0].offsetWidth + gap;
+    track.style.transform = `translateX(-${index * cardWidth}px)`;
+  }
+
+  if (track && cards.length && prev && next) {
+    next.addEventListener("click", () => {
+      if (index < cards.length - 1) {
+        index++;
+        updateCarousel();
+      }
+    });
+
+    prev.addEventListener("click", () => {
+      if (index > 0) {
+        index--;
+        updateCarousel();
+      }
+    });
+
+    window.addEventListener("resize", updateCarousel);
+    updateCarousel();
+  }
+
+  // Изменение языка в статьях
+
+  function markdownToHtml(md) {
+    if (!md) return "";
+    return md
+      .replace(/^### (.*$)/gim, '<h3 class="blog-post__title title">$1</h3>')
+      .replace(/^## (.*$)/gim, '<h2 class="blog-post__title title">$1</h2>')
+      .replace(/^# (.*$)/gim, '<h1 class="blog-post__title title">$1</h1>')
+      .replace(/\*\*(.+?)\*\*/gim, "<b>$1</b>")
+      .replace(/\*(.+?)\*/gim, "<i>$1</i>")
+      .replace(/^(?!<h|<p>)(.+)$/gim, '<p class="blog-post__text text">$1</p>')
+      .replace(/\n/gim, "<br>");
+  }
+
+  function loadArticle(lang) {
+    const articleContainer = document.getElementById("article-container");
+    if (articleContainer) {
+      const currentArticlesSlug = articleContainer.dataset.slug;
+      fetch(`articles/${currentArticlesSlug}/${lang}.md`)
+        .then((res) => (res.ok ? res.text() : Promise.reject("File missing")))
+        .then((md) => (articleContainer.innerHTML = markdownToHtml(md)))
+        .catch(
+          () =>
+            (articleContainer.innerHTML = `<p>Перевод для языка "${lang}" недоступен.</p>`),
+        );
+    }
+  }
+});
